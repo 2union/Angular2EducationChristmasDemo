9f397a75982f616ccc34262f1f5b80c624a698fa
Daniil Myasnikov <dan.myasnikov@gmail.com>
2016-12-27 09:58:00 +0500

    our first model

--- app/tasks/list.component.ts
+++ app/tasks/list.component.ts
@@ -1,42 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Person } from '../model/person';
 
 @Component({
   selector: 'list',
   templateUrl: './app/templates/list.html',
 })
-export class ListComponent  {
-  persons: Object[] = [
-    {
-      "id"  : 1,
-      "name": 'Ms. Jackson'
-    },
-    {
-      "id"  : 2,
-      "name": 'Mr. Philips'
-    },
-    {
-      "id"  : 3,
-      "name": 'Cusine Susy'
-    },
-    {
-      "id"  : 4,
-      "name": 'Antonio'
-    },
-    {
-      "id"  : 5,
-      "name": 'Carl'
-    },
-    {
-      "id"  : 6,
-      "name": 'Lisa'
-    }
-  ];
+export class ListComponent implements OnInit  {
+  persons: Person[];
 
   lastId: number = 6;
 
-  add(name: string = '') {
+  add(name: string = ''): boolean {
     this.lastId++;
-    let person: Object = {
+    let person: Person = {
       "id": this.lastId,
       "name": name
     };
@@ -44,11 +21,40 @@ export class ListComponent  {
     return false;
   }
 
-  delete(person: Object = null) {
+  delete(person: Person = null): void {
     let index = this.persons.indexOf(person);
 
     if ( index > -1 ) {
       this.persons.splice(index, 1);
     }
+  }
+
+  ngOnInit(): void {
+    this.persons = [
+      {
+        "id"  : 1,
+        "name": 'Ms. Jackson'
+      },
+      {
+        "id"  : 2,
+        "name": 'Mr. Philips'
+      },
+      {
+        "id"  : 3,
+        "name": 'Cusine Susy'
+      },
+      {
+        "id"  : 4,
+        "name": 'Antonio'
+      },
+      {
+        "id"  : 5,
+        "name": 'Carl'
+      },
+      {
+        "id"  : 6,
+        "name": 'Lisa'
+      }
+    ];
   }
 }
